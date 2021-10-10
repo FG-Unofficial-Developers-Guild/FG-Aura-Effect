16e52b248eab47e5e2466621ebba6b76fd4503b0
Wil Thieme <wil.thieme@protonmail.com>
2021-10-10 09:46:59 -0400

    Update manager_effect_aura.lua

--- scripts/manager_effect_aura.lua
+++ scripts/manager_effect_aura.lua
@@ -1,3 +1,7 @@
+--
+--	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
 OOB_MSGTYPE_AURATOKENMOVE = "aurasontokenmove";
 OOB_MSGTYPE_AURAAPPLYSILENT = "applyeffsilent";
 OOB_MSGTYPE_AURAEXPIRESILENT = "expireeffsilent";
