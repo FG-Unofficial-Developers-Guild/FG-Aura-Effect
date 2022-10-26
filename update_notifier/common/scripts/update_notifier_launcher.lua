547a821fb857b771d0e8ec78f978b7e39d9db406
Wil Thieme <wil.thieme@protonmail.com>
2022-10-26 12:35:55 -0400

    fix node location

--- update_notifier/common/scripts/update_notifier_launcher.lua
+++ update_notifier/common/scripts/update_notifier_launcher.lua
@@ -1,6 +1,6 @@
 function onInit()
     local nodeUpdateNotifier = DB.getRoot().createChild('update_notifier')
     if nodeUpdateNotifier and not nodeUpdateNotifier.getChild('bmosaurawishlist') then
-        Interface.openWindow('update_notifier', nodeUpdateNotifier.createChild('bmosaurawishlist'))
+        Interface.openWindow('update_notifier', nodeUpdateNotifier)
     end
 end
