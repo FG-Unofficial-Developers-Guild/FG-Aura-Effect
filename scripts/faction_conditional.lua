ae0a0c3d86e019077d48bebb597fccb4b5b37952
Ryan Hagelstrom <rhagelstrom@gmail.com>
2023-09-30 09:14:55 -0500

    Faction conditional updated to fall in line with FG conditional operator design. Fixes IFT not working correctly Support for !FACTION and self, !self and ~self. IF: FACTION(notself) is not removed when an aura effect is applied. Changed faction "faction" to "none" to fall in line with FG CT verbage. If CT token faction set to "none", will use source of effect faction in reguards to determining of factional relationship for purposes of applying FROMAURA

--- scripts/faction_conditional.lua
+++ scripts/faction_conditional.lua
@@ -1,93 +1,126 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-
--- luacheck: globals bDebug getRelationship checkFaction DetectedEffectManager
-
+-- luacheck: globals bDebug getRelationship DetectedEffectManager hasFaction customCheckConditional
 bDebug = false
 
 DetectedEffectManager = nil
 
 function getRelationship(rActor, rTarget)
-	if bDebug then Debug.chat('getRelationship:args', rActor, rTarget) end
-	local sourceFaction = ActorManager.getFaction(rActor)
-	local targetFaction = ActorManager.getFaction(rTarget)
-	if bDebug then Debug.chat('getRelationship:factions', sourceFaction, targetFaction) end
-	if sourceFaction == targetFaction then
-		return 'ally' -- actors are allies when their factions match
-	elseif sourceFaction == 'friend' and targetFaction == 'foe' then
-		return 'enemy' -- actors are enemies when one is a friend and one is a foe
-	elseif sourceFaction == 'foe' and targetFaction == 'friend' then
-		return 'enemy' -- actors are enemies when one is a friend and one is a foe
-	else
-		return ''
-	end
+    if bDebug then
+        Debug.chat('getRelationship:args', rActor, rTarget)
+    end
+    local sourceFaction = ActorManager.getFaction(rActor)
+    local targetFaction = ActorManager.getFaction(rTarget)
+    if bDebug then
+        Debug.chat('getRelationship:factions', sourceFaction, targetFaction)
+    end
+    if sourceFaction == targetFaction then
+        return 'ally' -- actors are allies when their factions match
+    elseif sourceFaction == 'friend' and targetFaction == 'foe' then
+        return 'enemy' -- actors are enemies when one is a friend and one is a foe
+    elseif sourceFaction == 'foe' and targetFaction == 'friend' then
+        return 'enemy' -- actors are enemies when one is a friend and one is a foe
+    else
+        return ''
+    end
 end
 
-function checkFaction(rActor, rTarget, aFactions)
-	local bReturn = false
-	if bDebug then Debug.chat('checkFaction:args', rActor, rTarget, aFactions) end
-	if not rActor or not next(aFactions) then return false end
-	local bNegate
-	for _, sFaction in ipairs(aFactions) do
-		bNegate = false
-		if StringManager.startsWith(sFaction, '!') or StringManager.startsWith(sFaction, '~') then
-			sFaction = sFaction:sub(2)
-			bNegate = true
-		end
-		if not bNegate and sFaction == 'all' then
-			bReturn = true
-			break
-		elseif not bNegate and StringManager.contains({ ActorManager.getFaction(rActor), getRelationship(rActor, rTarget) }, sFaction) then
-			bReturn = true
-			break
-		elseif bNegate and not StringManager.contains({ ActorManager.getFaction(rActor), getRelationship(rActor, rTarget) }, sFaction) then
-			bReturn = true
-			break
-		end
-	end
-	if bDebug then Debug.chat('checkFaction:results', bReturn, 'negation:', bNegate) end
+function hasFaction(rActor, sFaction, rTarget, nodeEffect)
+    local bReturn = false
+    local bNegate = false
+    local sTargetFaction
+    local sTargetNode
+    local aFactions = StringManager.splitByPattern(sFaction, '%s*,%s*', true)
+    local sNodeEffectSource = DB.getValue(nodeEffect, 'source_name', DB.getPath(DB.getChild(nodeEffect, '...')))
 
-	return bReturn
-end
+    -- If there is a target IFT. No Target IF
+    if rTarget then
+        sTargetNode = rTarget.sCTNode
+        sTargetFaction = ActorManager.getFaction(rTarget)
+    else
+        sTargetNode = rActor.sCTNode
+        sTargetFaction = ActorManager.getFaction(rActor)
+    end
 
-local function checkFactionConditional(rActor, nodeEffect, aConditions)
-	if aConditions and aConditions.remainder then aConditions = aConditions.remainder end
-	local rAuraSource = ActorManager.resolveActor(DB.getValue(nodeEffect, 'source_name', ''))
-	for _, v in ipairs(aConditions) do
-		local sFactionCheck = v:lower():match('^faction%s*%(([^)]+)%)$')
-		if sFactionCheck then
-			if not checkFaction(rActor, rAuraSource, {sFactionCheck}) then return false end
-		end
-	end
-	return true
+    for _, sFactionElement in ipairs(aFactions) do
+        if StringManager.startsWith(sFactionElement, '!') or StringManager.startsWith(sFactionElement, '~') then
+            sFactionElement = sFactionElement:sub(2)
+            bNegate = true
+        end
+        if sFactionElement == 'all' then
+            bReturn = true
+            break
+        end
+        if sFactionElement == 'notself' and ((not bNegate and sTargetNode ~= sNodeEffectSource) or (bNegate and sTargetNode == sNodeEffectSource)) then
+            bReturn = true
+            break
+        end
+        if sFactionElement == 'self' and ((not bNegate and sTargetNode == sNodeEffectSource) or (bNegate and sTargetNode ~= sNodeEffectSource)) then
+            bReturn = true
+            break
+        end
+        if sFactionElement == 'ally' or sFactionElement == 'enemy' then
+            local sFactionRelationship = getRelationship(rActor, rTarget)
+            if (not bNegate and sFactionElement == sFactionRelationship) or (bNegate and sFactionElement ~= sFactionRelationship) then
+                bReturn = true
+                break
+            end
+        end
+        if (sFactionElement == 'foe' or sFactionElement == 'friend' or sFactionElement == 'neutral') and
+            ((not bNegate and sFactionElement == sTargetFaction) or (bNegate and sFactionElement ~= sTargetFaction)) then
+            bReturn = true
+            break
+        end
+        if sFactionElement == 'none' and ((not bNegate and sTargetFaction == '') or (bNegate and sTargetFaction ~= '')) then
+            bReturn = true
+            break
+        end
+
+    end
+    return bReturn
 end
 
 local checkConditional_old
-local function checkConditional_new(rActor, nodeEffect, aConditions, rTarget, aIgnore, ...)
-	if bDebug then Debug.chat('checkConditional_new:args', rActor, nodeEffect, aConditions, rTarget, aIgnore, ...) end
+function customCheckConditional(rActor, nodeEffect, aConditions, rTarget, aIgnore)
+    local bReturn = checkConditional_old(rActor, nodeEffect, aConditions, rTarget, aIgnore);
+    if bReturn then
+        for _, v in ipairs(aConditions) do
+            local sLower = v:lower()
+            local bNegate = false
+            if StringManager.startsWith(sLower, '!') or StringManager.startsWith(sLower, '~') then
+                sLower = sLower:sub(2)
+                bNegate = true
+            end
+            local sFaction = sLower:match('^faction%s*%(([^)]+)%)$')
 
-	local bReturn = checkConditional_old(rActor, nodeEffect, aConditions, rTarget, aIgnore, ...)
-	if not bReturn then return bReturn end -- skip faction check if conditions already aren't passing
-
-	return checkFactionConditional(rActor, nodeEffect, aConditions)
+            if sFaction then
+                local bHasFaction = hasFaction(rActor, sFaction, rTarget, nodeEffect)
+                if (not bNegate and not bHasFaction) or (bNegate and bHasFaction) then
+                    bReturn = false
+                    break
+                end
+            end
+        end
+    end
+    return bReturn
 end
 
 function onInit()
-	-- Set up the effect manager proxy functions for the detected ruleset
-	if EffectManager35E then
-		DetectedEffectManager = EffectManager35E
-	elseif EffectManagerPFRPG2 then
-		DetectedEffectManager = EffectManagerPFRPG2
-	elseif EffectManagerSFRPG then
-		DetectedEffectManager = EffectManagerSFRPG
-	elseif EffectManager5E then
-		DetectedEffectManager = EffectManager5E
-	elseif EffectManager4E then
-		DetectedEffectManager = EffectManager4E
-	end
+    -- Set up the effect manager proxy functions for the detected ruleset
+    if EffectManager35E then
+        DetectedEffectManager = EffectManager35E
+    elseif EffectManagerPFRPG2 then
+        DetectedEffectManager = EffectManagerPFRPG2
+    elseif EffectManagerSFRPG then
+        DetectedEffectManager = EffectManagerSFRPG
+    elseif EffectManager5E then
+        DetectedEffectManager = EffectManager5E
+    elseif EffectManager4E then
+        DetectedEffectManager = EffectManager4E
+    end
 
-	-- create proxy function to add FACTION conditional
-	checkConditional_old = DetectedEffectManager.checkConditional
-	DetectedEffectManager.checkConditional = checkConditional_new
+    -- create proxy function to add FACTION conditional
+    checkConditional_old = DetectedEffectManager.checkConditional
+    DetectedEffectManager.checkConditional = customCheckConditional
 end
