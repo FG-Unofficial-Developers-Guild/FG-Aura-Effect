6a0b52f399e39ba17c37cf6ae77cdaa64e2d6615
Wil Thieme <wil.thieme@protonmail.com>
2023-12-17 08:14:51 -0500

    revise effect builder labels

--- effect_builder/effects/scripts/AURA.lua
+++ effect_builder/effects/scripts/AURA.lua
@@ -1,12 +1,12 @@
--- luacheck: globals createEffectString parentcontrol number_value effect_faction effect_auraSingle effect_auraCube
+-- luacheck: globals createEffectString parentcontrol number_value effect_auraFaction effect_auraSingle effect_auraCube
 -- luacheck: globals effect_auraSticky
 
 function createEffectString()
 	local effectString = parentcontrol.window.effect.getStringValue() .. ': ' .. number_value.getStringValue()
-	if not effect_faction.isEmpty() then effectString = effectString .. ' ' .. effect_faction.getStringValue() end
+	if not effect_auraFaction.isEmpty() then effectString = effectString .. ' ' .. effect_auraFaction.getStringValue() end
 
-	if not effect_faction.isEmpty() and effect_auraSingle.getValue() > 0 then effectString = effectString .. ',single' end
-	if not effect_faction.isEmpty() and effect_auraCube.getValue() > 0 then effectString = effectString .. ',cube' end
-	if not effect_faction.isEmpty() and effect_auraSticky.getValue() > 0 then effectString = effectString .. ',sticky' end
+	if not effect_auraFaction.isEmpty() and effect_auraSingle.getValue() > 0 then effectString = effectString .. ',single' end
+	if not effect_auraFaction.isEmpty() and effect_auraCube.getValue() > 0 then effectString = effectString .. ',cube' end
+	if not effect_auraFaction.isEmpty() and effect_auraSticky.getValue() > 0 then effectString = effectString .. ',sticky' end
 	return effectString
 end
