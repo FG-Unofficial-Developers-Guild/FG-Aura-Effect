0929ba984a3322f7dbf8002ace0115abd745464a
Ryan Hagelstrom <rhagelstrom@gmail.com>
2024-05-29 21:35:34 -0500

    Added descriptor dying and which will only apply applied aura if source is dying or if not dying Added descriptors and checks for applied effects for CreatureTypes, CreatureSize, Alignment

--- scripts/aura_triggers.lua
+++ scripts/aura_triggers.lua
@@ -6,7 +6,7 @@
 -- luacheck: globals updateAurasForMap updateAurasForActor updateAurasForEffect addEffect_new
 -- luacheck: globals updateAurasForTurnStart AuraEffect.clearOncePerTurn AuraTracker AuraToken
 -- luacheck: globals AuraFactionConditional.DetectedEffectManager.parseEffectComp hasExtension
--- luacheck: globals linkToken_new onTokenDelete_new
+-- luacheck: globals linkToken_new onTokenDelete_new onHeathUpdate
 
 bDebug = false
 bDebugPerformance = false
@@ -246,6 +246,13 @@ local function onEffectRemoved(nodeEffects)
 	AuraEffectTriggers.updateAurasForActor(DB.getParent(nodeEffects))
 end
 
+local function onHealthUpdate(nodeHP)
+	local nodeActor = DB.getParent(nodeHP)
+	local _, window = ImageManager.getImageControl(CombatManager.getTokenFromCT(DB.getParent(nodeHP)))
+
+	updateAurasForActor(nodeActor, window)
+end
+
 function onInit()
 	tExtensions = AuraEffectTriggers.getExtensions()
 	-- all handlers should be created on GM machine
@@ -256,6 +263,7 @@ function onInit()
 	DB.addHandler(DB.getPath(CombatManager.CT_LIST .. '.*.effects.*.isactive'), 'onUpdate', onEffectChanged)
 	DB.addHandler(DB.getPath(CombatManager.CT_LIST .. '.*.effects.*'), 'onDelete', onEffectToBeRemoved)
 	DB.addHandler(DB.getPath(CombatManager.CT_LIST .. '.*.effects'), 'onChildDeleted', onEffectRemoved)
+	DB.addHandler(DB.getPath(CombatManager.CT_LIST .. '.*.wounds'), 'onUpdate', onHealthUpdate)
 
 	-- create the proxy function to trigger aura calculation on token movement.
 	Token.addEventHandler('onMove', onMove)
